8e2a9afc451633c3ec017689bc23d9982d64bc49
Hilary Ogutu <97037614+ogutuhill@users.noreply.github.com>
2024-12-04 11:32:35 +0300

    Update gradeGenerator.js

--- assignment/gradeGenerator.js
+++ assignment/gradeGenerator.js
@@ -1,30 +1,45 @@
-function gradeStudentMarks() {
-    let marks = prompt("Enter the student's marks (between 0 and 100):");   //User is prompted to enter marks
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
 
-    marks = Number(marks); //Input must be a number
+function gradeStudentMarks(marks) {
+    studentGrade = parseFloat(marks);
 
 //Input is validation
-    if (isNaN(marks) || marks < 0 || marks > 100) {
-        alert("Invalid input. Please enter a number between 0 and 100.");
-        return;
+    if (isNaN(studentGrade) || studentGrade < 0 || studentGrade > 100) {
+        console.log("Invalid input. Please enter a number between 0 and 100.");
+        
     }
 
     // Determines the grade based on the marks inputs
-    let grade;
     if (marks > 79) {
-        grade = "A";
-    } else if (marks >= 60) {
-        grade = "B";
-    } else if (marks >= 50) {
-        grade = "C";
-    } else if (marks >= 40) {
-        grade = "D";
-    } else {
-        grade = "E";
-    }
+        console.log("Grade: A");
 
-     alert(`The grade for marks ${marks} is: ${grade}`);  // didplays the grade
+    } else if (marks >= 60) {
+        console.log("Grade: B")
+
+    } else if (marks >= 50) {
+        console.log("Grade: C");
+
+    } else if (marks >= 40) {
+        console.log("Grade: D");
+
+    } else {
+        console.log("Grade: E");
+    }
 }
 
- gradeStudentMarks();  //invokes the function
-
+function promptUser() {
+    rl.question("Enter the marks now (0-100) or press esc' to quit: ", (input) => {
+        if (input.toLowerCase() === 'exit') {
+            console.log("Exiting the program.");
+            rl.close(); //closing input prompt
+        } else {
+            grading(input); 
+            promptUser(); // calling itself to loop
+        }
+    });
+}
+promptUser();
